21889ea2e4bd67538d09c3918a7c47872286dbde
Kateřina Klimková <katerina.klimkova@email.cz>
2024-08-29 05:38:15 +0200

    Vytvoření třídy Brand v models.py

--- viewer/models.py
+++ viewer/models.py
@@ -2,3 +2,10 @@ from django.db.models import (
     DO_NOTHING, CharField, DateField, DateTimeField, ForeignKey, IntegerField,
     Model, TextField
 )
+
+class Brand(Model):
+    name_brand = CharField(max_length=50)
+    description = TextField()
+
+    def __str__(self):
+        return f"Značka automobilu: {self.name_brand}\nPředstavení značky: {self.description}"
